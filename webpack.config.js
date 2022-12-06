afa4b1751d275209a3d872596b72b27ad2f77ac3
Sidorov Maksim <maxx11167@gmail.com>
2022-12-06 18:59:49 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ]
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "main.html"
+        }),
+
+        new MiniCssExtractPlugin(),
+    ]
+    
+}
